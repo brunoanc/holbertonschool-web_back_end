aa08bf63c1134c6bd6831a23dd1b4eb0efa53fae
Bruno Ancona <brunoanconasala@gmail.com>
2026-02-09 08:54:34 -0600

    Task 1

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Create table
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
