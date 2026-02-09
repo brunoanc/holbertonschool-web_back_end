a49ba06441b5c22bd02c306cd11e99b07846e407
Bruno Ancona <brunoanconasala@gmail.com>
2026-02-09 12:00:49 -0600

    Task 7

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Create procedure to calculate average score
+DELIMITER |
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN in_user_id INT
+)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT AVG(score) FROM corrections WHERE user_id = in_user_id
+    )
+    WHERE id = in_user_id;
+END
+|
+
+DELIMITER ;
